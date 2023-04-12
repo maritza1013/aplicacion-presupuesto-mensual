0ab9db02208db23d1b5f24572dec15426b550d40
maritza1013 <maritzamae13@gmail.com>
2023-04-12 15:00:22 -0500

    aplicando el formato de la moneda y porcentaje con JavaScript

--- js/app.js
+++ js/app.js
@@ -7,3 +7,40 @@ const egresos = [
     new Egreso('Renta departamento', 900),
     new Egreso('Ropa', 400)
 ];
+
+let cargarApp = ()=>{
+    cargarCabecero();
+}
+
+let totalIngresos = ()=>{
+    let totalIngreso = 0;
+    for (let ingreso of ingresos){
+    totalIngreso += ingreso.valor;
+} 
+    return totalIngreso;
+}
+
+let totalEgresos = ()=>{
+    let totalEgreso = 0;
+    for(let egreso of egresos){
+    totalEgreso += egreso.valor;
+    }
+    return totalEgreso;
+}
+
+let cargarCabecero = ()=>{
+    let presupuesto = totalIngresos() - totalEgresos();
+    let porcentajeEgresos = totalEgresos()/totalIngresos();
+    document.getElementById('presupuesto').innerHTML = formatoMoneda(presupuesto);
+    document.getElementById('porcentaje').innerHTML = formatoPorcentaje(porcentajeEgresos);
+    document.getElementById('ingresos').innerHTML = formatoMoneda(totalIngresos());
+    document.getElementById('egresos').innerHTML = formatoMoneda(totalEgresos());
+}
+
+const formatoMoneda = (valor)=>{
+    return valor.toLocaleString('en-US',{style:'currency',currency:'USD', minimumFractionDigits:2})
+}
+
+const formatoPorcentaje = (valor)=>{
+    return valor.toLocaleString('en-US', {style:'percent', minimumFractionDigits:2});
+}
